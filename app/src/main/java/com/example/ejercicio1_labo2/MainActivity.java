2411286a66a18dfd138bab856b581df3798c8f4d
calderonra <00004017@uca.edu.sv>
2019-03-15 10:26:11 -0600

    Funcionalidades con los botones corregidas

--- app/src/main/java/com/example/ejercicio1_labo2/MainActivity.java
+++ app/src/main/java/com/example/ejercicio1_labo2/MainActivity.java
@@ -4,36 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button entry_btn;
     private String shortClick;
-    private TextView Palabra;
+    private TextView mensaje;
+    private EditText usuario;
+    private String aux;
+    private EditText contra;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         entry_btn = findViewById(R.id.boton);
-        Palabra=findViewById(R.id.mensaje);
+        mensaje = findViewById(R.id.mensaje);
+        usuario = findViewById(R.id.user);
+        contra = findViewById(R.id.contraseña);
 
         entry_btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                shortClick="username";
-                Palabra.setText(shortClick);
+                aux=usuario.getText().toString();
+                shortClick=aux;
+                mensaje.setText(shortClick);
             }
         });;
 
         entry_btn.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                shortClick="password";
-                Palabra.setText(shortClick);
+                aux=contra.getText().toString();
+                shortClick=aux;
+                mensaje.setText(shortClick);
                 return true;
             }
         });
-
 
     }
 
